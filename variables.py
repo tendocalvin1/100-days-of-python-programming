244523ea1cd157d4b0e74164c8d045b84193d277
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2025-09-08 09:57:41 +0300

    Doing some exercises on Variables

--- variables.py
+++ variables.py
@@ -16,9 +16,34 @@ print("height:", height)
 
 # input in python
 # The input() function allows  the user to type something from the keyboard
-name = input("Enter your name please:")
-print("My name is:", name)
+#name = input("Enter your name please:")
+#print("My name is:", name)
 
 # By default, input is read as a string. If you want numbers, you must convert:
-age = int(input("Enter your age please:")) # converts input to string
-print("I shall be", age + 1, "years old next year.")
+#age = int(input("Enter your age please:")) # converts input to string
+#print("I shall be", age + 1, "years old next year.")
+
+
+# output in python
+# we use print for output in python
+#name = "Tendo Calvin SWE"
+#score = 90
+#print(f"Hello {name}, your score is {score}")
+
+
+
+
+# Exercises
+# 1. Greeting program
+name = input("Enter your name please:")
+print(f"Hello {name}, welcome to python programming.")
+
+# 2. Age in 5 years.
+age = int(input("Enter the age:"))
+print("I shall be", age + 5,"in the next 5 years")
+
+# simple calculator
+number_one = int(input("Enter the first number:"))
+number_two = int(input("Enter the second number:"))
+
+print(f"The sum of these two numbers is:")
