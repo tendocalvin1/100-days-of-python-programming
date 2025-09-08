2b62d8e7c31a9b8c4e153c4f9af2efe90d02ad25
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2025-09-08 13:46:47 +0300

    I have covered some of the basics of Python programming languange like variables through doing some exercises as well

--- variables.py
+++ variables.py
@@ -35,15 +35,59 @@ print("height:", height)
 
 # Exercises
 # 1. Greeting program
-name = input("Enter your name please:")
-print(f"Hello {name}, welcome to python programming.")
+# name = input("Enter your name please:")
+# print(f"Hello {name}, welcome to python programming.")
 
-# 2. Age in 5 years.
-age = int(input("Enter the age:"))
-print("I shall be", age + 5,"in the next 5 years")
+# # 2. Age in 5 years.
+# age = int(input("Enter the age:"))
+# print("I shall be", age + 5,"in the next 5 years")
 
-# simple calculator
-number_one = int(input("Enter the first number:"))
-number_two = int(input("Enter the second number:"))
+# # simple calculator
+# number_one = int(input("Enter the first number:"))
+# number_two = int(input("Enter the second number:"))
 
-print(f"The sum of these two numbers is:")
+# print(f"The sum of these two numbers is:",number_one + number_two)
+# print(f"The product of these two numbers is:",number_one * number_two)
+# print(f"The result of the division of these two numbers is:",number_one / number_two)
+# print(f"The result from the subtraction of these two numbers is:",number_one - number_two)
+
+# Area of a rectangle
+# length = int(input("Enter the length:"))
+# width = int(input("Enter the width:"))
+# area = length* width
+# print(f"The area of the rectangle is {area} cm\u00b2")
+
+
+# Temperature converter
+# Temps_in_celsius = int(input("Enter the degrees(temps):"))
+# temps_in_fahreinheit = (Temps_in_celsius * 1.8) + 32
+# print(f"The temps from celsius to fahreinheit are:", temps_in_fahreinheit)
+
+
+
+# even or odd number
+# number = int(input("Enter a digit:"))
+# if number % 2 == 0:
+#     print("This is an even number")
+
+# else:
+#     print("This is an odd number")
+
+# Swapping Variables
+# Write a program that swaps the values of two variables. (e.g., a=5, b=10 → after swap → a=10, b=5)
+
+a = 5
+b = 10
+a, b = b, a  # swap values
+print("a =", a, "b =", b)
+
+# Simple Interest Calculator
+# Ask for principal, rate, and time, then calculate Simple Interest using:
+# SI = (P × R × T) / 100
+
+pricipal = 500
+rate = 0.5
+time = 3
+
+simple_interest = pricipal * rate * time
+print(f"The simple interest from this man's account is:", simple_interest)
