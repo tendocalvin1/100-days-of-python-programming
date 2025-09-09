fac0fe8d11fb5bc7c38b6078c307f4f6ae4fd2a5
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2025-09-09 19:07:57 +0300

    Learnt the different data types of pthon as a programming language

---
+++ data-types.py
@@ -0,0 +1,33 @@
+# Day two of 100 days of python code
+# integers [Whole numbers, positive or negative without decimals]
+age = 23
+year = 2025
+temperature = -7
+
+print(age)
+print(year)
+print(temperature)
+
+# Float [Numbers with decimal points]
+pi = 3.14159
+price = 19.99
+height = 1.75
+
+print(pi)
+print(price)
+print(height)
+
+
+# Strings (str)
+# Text enclosed in single '' or double " " quotes
+name = "Tendo Calvin SWE"
+city = "Mukono"
+greeting = "Hello, world !"
+
+
+# Boolean(bool)
+# Represents True or False
+# Used for conditions, logic or comparisons
+is_raining = True
+has_license = False
+passed_exam = True
