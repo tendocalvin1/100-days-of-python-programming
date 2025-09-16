bd8b460d2f7342e8c36931a02dd4920a478b5a11
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2025-09-16 16:22:52 +0300

    I have done a simple exercise combining variables and input/output

---
+++ Exercises.py
@@ -0,0 +1,21 @@
+# Write a program that asks the user for their name and age, then prints a greeting:
+# Example: “Hello John, you are 20 years old.”
+
+name = str(input("What is your name please ?"))
+age = int(input("What is your age please ?"))
+print(f"Hello, my name is {name} and I am {age} years old")
+
+# Swap the values of two variables (without using a third variable).
+# numbers 
+# ['a', 'b'] = [10, 15]
+# print('a', 'b')
+
+# Take two numbers from the user and print their sum, difference, product, and quotient.
+
+number_one = int(input("Enter the first number please:"))
+number_two = int(input("Enter the second number please:"))
+
+print(f"The sum of these two numbers is :", number_one + number_two)
+print(f"The difference of these two numbers is :", number_one - number_two)
+print(f"The product of these two numbers is :", number_one * number_two)
+print(f"Result from the division between these two numbers is :", number_one / number_two)
