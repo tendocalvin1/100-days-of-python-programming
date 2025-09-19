e988088873971ef21ea805256e9d28a9fc67e391
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2025-09-19 23:27:01 +0300

    made a few changes to the file

--- functions.py
+++ functions.py
@@ -30,3 +30,46 @@ def calculate(a, b):
     print(sum_result, difference)
     
 calculate(6, 4)
+
+# Exercises on Functions in python
+# Write a function square(num) that takes a number and returns its square.
+
+def square(number):
+    print(number ** 2)
+
+square(6)
+square(4)
+
+# Write a function is_even(num) that returns True if a number is even, otherwise False.
+def is_even(num):
+    if num % 2 == 0:
+        print("The number is even")
+        
+    else:
+        print("The number is odd")
+        
+is_even(3)
+is_even(4)
+
+# Write a function factorial(n) that calculates the factorial of a number.
+def factorial(n):
+    numbers = [1,2,3,4,5]
+for number in numbers:
+    print("5*4*3*2*1")
+    
+factorial(5)
+    
+#     # looping through a list
+# fruits = ["apple", "banana", "cherry"]
+# for fruit in fruits:
+#     print(fruit)
+
+# Write a function reverse_string(text) that returns the string reversed.
+
+
+
+# Write a function average(numbers) that takes a list of numbers and returns their average.
+
+
+
+ 
