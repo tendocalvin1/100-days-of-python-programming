25d4396dd14e548aeccf6bd2bbae176d4e47fa5f
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2025-09-30 10:51:16 +0300

    I am learning one of the core principles of OOP in any programming languange that is Inheritance

---
+++ inheritance.py
@@ -0,0 +1,7 @@
+# object = A bundle of related attributes (variables) and methods(functions)
+#          Example: phone, cup, book
+#          You need a class to create objects
+
+
+# class = A blueprint used to design the structure and layout of an object
+
