1aeab1c1b7752ba83b64d228b09f2d622984821b
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2025-10-02 12:19:41 +0300

    Learning about composition and aggregation in OOP

---
+++ composition.py
@@ -0,0 +1,1 @@
+# composition in OOP
--- polymorphism.py
+++ polymorphism.py
@@ -53,3 +53,102 @@ shapes = [Circle(5), Square(10), Triangle(4, 8), Rectangle(10, 15)]
 
 for shape in shapes:
     print(f"{shape.area()}")
+    
+    
+# Duck typing = Another way to achieve polymorphism besides Inheritance
+#               Object must have the minimum necessary attributes/methods
+#               "If it looks like a duck and quacks like a duck, it must be a duck"
+
+
+class Animal:
+    alive = True
+    
+
+class Dog(Animal):
+    def speak(self):
+        print("WOOF !")
+        
+
+class Cat(Animal):
+    def speak(self):
+        print("MEOW !")
+        
+        
+class Car:
+    alive = False
+    def speak(self):
+        print("HORN !")
+        
+        
+animals = [Dog(), Cat(), Car()]
+for animal in animals:
+    animal.speak()
+    print(animal.alive)
+    
+    
+
+# Aggregation = Represents a relationship where one object(the whole) contains references
+#               to one or more INDEPENDENT objects (the parts)
+
+class Library:
+    def __init__(self, name):
+        self.name = name
+        self.books = []
+        
+    def add_book(self, book):
+        self.books.append(book)
+        
+    
+    def list_book(self):
+        return [f"{book.title} by {book.author}" for book in self.books ]
+        
+        
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        
+library = Library("UCU Public Library")
+
+book1 = Book("Diary of a ceo", "Steve Bartlett") 
+book2 = Book("Good to Great", "Jim Collins") 
+book3 = Book("How The Mighty Fall", "Jim Collins") 
+
+
+library.add_book(book1)
+library.add_book(book2)
+library.add_book(book3)
+
+print(library.name)
+
+for book in library.list_book():
+    print(book)
+    
+    
+# composition = The composed object directly owns its components, which cannot exist independently
+#               "owns a relationship"
+
+class Engine:
+    def __init__(self, horse_power):
+        self.horse_power = horse_power
+        
+class Wheel:
+    def __init__(self, size):
+        self.size = size
+        
+class Vehicle:
+    def __init__(self, make, model, horse_power, wheel_size):
+        self.make = make
+        self.model = model
+        self.engine = Engine(horse_power)
+        self.wheels = [Wheel(wheel_size) for wheel in range(4)]
+        
+    def description(self):
+        return f"{self.make} {self.model}  {self.engine.horse_power}(hp) {self.wheels[0].size}inch"
+        
+
+vehicle1 = Vehicle("Ford", "Mustang", 500, 18)
+vehicle2 = Vehicle("Toyota", "Harrier", 650, 19)
+
+print(vehicle1.description())
+print(vehicle2.description())
