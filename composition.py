f3a75ac62a45b15cd6d21fe696d29f1e41aad8f5
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2025-10-03 15:24:44 +0300

    learning composition and aggregation in OOP

--- composition.py
+++ composition.py
@@ -1,1 +1,5 @@
 # composition in OOP
+# ✅ Composition (“Strong has-a” relationship)
+# The whole object owns its parts.
+# If the whole is destroyed, the parts are also destroyed.
+# The part cannot meaningfully exist without the whole.
